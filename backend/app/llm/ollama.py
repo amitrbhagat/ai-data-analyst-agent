f846e3bd6a0fbafa47a057468127c3132e6fe9fa
Amit <amitrbhagat6789@gmail.com>
2026-08-18 12:05:34 -0700

    feat(llm): add instruction-based prompt structure to generate_response

---
+++ backend/app/llm/ollama.py
@@ -0,0 +1,49 @@
+import requests
+
+
+def generate_response(prompt:str, temperature: float = 0.7) -> str:
+
+    url = "http://localhost:11434/api/chat"
+
+    payload = {
+        "model" : "tinyllama",
+
+        "messages": [
+            {"role":"user", "content":prompt}
+        ],
+
+        "options": {
+            "temperature": temperature
+        },
+        "stream":False
+    }
+
+
+    try :
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+
+        data = response.json()
+        # print(f"data is : {data}")
+        # print(data["created_at"])
+
+        generated_text = data["message"]["content"]
+        return generated_text
+
+    except requests.exceptions.RequestException as e:
+
+        print(f"Error occured {e}")   
+        return ""
+
+
+if __name__ == "__main__":
+
+    my_prompt = """
+    Instruction: Give the accurate answer
+    Question: What is the capital of India
+    """
+
+    res = generate_response(my_prompt, temperature=0.3)
+    print("-----------------")
+    print(res)
+            
---
+++ backend/app/llm/provider.py
@@ -0,0 +1,4 @@
+## In provider.py we are going to buld a abstraction layer 
+## where our rest of the application is going to connect with AI model (LLM) through this abstraction layer
+
+
